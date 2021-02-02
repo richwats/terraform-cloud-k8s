e12510ec44f343575203ae5d74fb3c086d9a7f2d
Richard <richwats@cisco.com>
2021-02-03 09:30:17 +1100

    EKS test 1

--- main.tf
+++ main.tf
@@ -78,10 +78,77 @@ provider "aws" {
 #   value = module.cloud-aci.aws-syd-reg
 # }
 
-module "aci_demo" {
-  source  = "app.terraform.io/mel-ciscolabs-com/aci-demo/cloud"
-  version = "1.0.0"
 
-  vault_password = var.vault_password
-  vault_username = var.vault_username
+## Data - Existing Prod VPC
+data "aws_vpc" "prod-vpc" {
+  cidr_block = "10.111.0.0/16"
+  # id = var.aws-vpc-id
+  # Tag? - Name?
+}
+
+## Data - Existing Prod Subnets
+data "aws_subnet" "eks-1" {
+  vpc_id = data.aws_vpc.prod-vpc.id
+  cidr_block = "10.111.3.0/24"
+}
+
+data "aws_subnet" "eks-2" {
+  vpc_id = data.aws_vpc.prod-vpc.id
+  cidr_block = "10.111.4.0/24"
+}
+
+## IAM Role for EKS
+resource "aws_iam_role" "tf-eks-role" {
+  name = "tf-eks-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.tf-eks-role.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.tf-eks-role.name
+}
+
+resource "aws_eks_cluster" "tf-eks-1" {
+  name     = "tf-eks-1"
+  role_arn = aws_iam_role.tf-eks-role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.eks-1.id, aws_subnet.eks-2.id]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.tf-eks-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.tf-eks-AmazonEKSVPCResourceController,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.tf-eks-1.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.tf-eks-1.certificate_authority[0].data
 }
