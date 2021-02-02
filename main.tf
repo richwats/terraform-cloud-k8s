dc9668660913a62c3ece9f5bbee4d2d7f2883ff7
Richard <richwats@cisco.com>
2021-02-03 09:36:35 +1100

    test with own role

--- main.tf
+++ main.tf
@@ -97,41 +97,42 @@ data "aws_subnet" "eks-2" {
   cidr_block = "10.111.4.0/24"
 }
 
-## IAM Role for EKS
-resource "aws_iam_role" "tf-eks-role" {
-  name = "tf-eks-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.tf-eks-role.name
-}
-
-# Optionally, enable Security Groups for Pods
-# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
-resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.tf-eks-role.name
-}
+# ## IAM Role for EKS
+# resource "aws_iam_role" "tf-eks-role" {
+#   name = "tf-eks-role"
+#
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+# }
+#
+# resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSClusterPolicy" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#   role       = aws_iam_role.tf-eks-role.name
+# }
+#
+# # Optionally, enable Security Groups for Pods
+# # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+# resource "aws_iam_role_policy_attachment" "tf-eks-AmazonEKSVPCResourceController" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+#   role       = aws_iam_role.tf-eks-role.name
+# }
 
 resource "aws_eks_cluster" "tf-eks-1" {
   name     = "tf-eks-1"
-  role_arn = aws_iam_role.tf-eks-role.arn
+  # role_arn = aws_iam_role.tf-eks-role.arn
+
 
   vpc_config {
     subnet_ids = [data.aws_subnet.eks-1.id, data.aws_subnet.eks-2.id]
@@ -139,10 +140,10 @@ resource "aws_eks_cluster" "tf-eks-1" {
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.tf-eks-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.tf-eks-AmazonEKSVPCResourceController,
-  ]
+  # depends_on = [
+  #   aws_iam_role_policy_attachment.tf-eks-AmazonEKSClusterPolicy,
+  #   aws_iam_role_policy_attachment.tf-eks-AmazonEKSVPCResourceController,
+  # ]
 }
 
 output "endpoint" {
