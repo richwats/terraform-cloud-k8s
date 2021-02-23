df820fd00012b5547b856b7ec00a72e768ec6ad4
Richard <richwats@cisco.com>
2021-02-24 09:16:46 +1100

    AKS test

--- main.tf
+++ main.tf
@@ -297,7 +297,7 @@ provider azurerm {
 #
 data "azuread_group" "cluster-admins" {
   display_name     = "ManualAKSClusterAdmins"
-  security_enabled = true
+  # security_enabled = true
 }
 
 data "azurerm_virtual_network" "tf-hc-prod" {
@@ -305,46 +305,46 @@ data "azurerm_virtual_network" "tf-hc-prod" {
   resource_group_name = data.azurerm_resource_group.tf-hc-prod.name
 }
 
-module "aks" {
-  source                           = "Azure/aks/azurerm"
-  resource_group_name              = data.azurerm_resource_group.tf-hc-prod.name
-  client_id                        = data.vault_generic_secret.azure.data["client_id"]
-  client_secret                    = data.vault_generic_secret.azure.data["secret"]
-  kubernetes_version               = "1.19.3"
-  orchestrator_version             = "1.19.3"
-  prefix                           = "tf-aks"
-  network_plugin                   = "azure"
-  vnet_subnet_id                   = data.azurerm_virtual_network.tf-hc-prod.id
-  os_disk_size_gb                  = 50
-  sku_tier                         = "Paid" # defaults to Free
-  enable_role_based_access_control = true
-  rbac_aad_admin_group_object_ids  = [data.azuread_group.cluster-admins.id]
-  rbac_aad_managed                 = true
-  private_cluster_enabled          = true # default value
-  enable_http_application_routing  = true
-  enable_azure_policy              = true
-  enable_auto_scaling              = true
-  agents_min_count                 = 1
-  agents_max_count                 = 2
-  agents_count                     = null # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
-  agents_max_pods                  = 100
-  agents_pool_name                 = "exnodepool"
-  # agents_availability_zones        = ["1", "2"]
-  agents_type                      = "VirtualMachineScaleSets"
-  agents_size                      = "Standard DS1 v2"
-
-  agents_labels = {
-    "nodepool" : "defaultnodepool"
-  }
-
-  agents_tags = {
-    "Agent" : "defaultnodepoolagent"
-  }
-
-  network_policy                 = "azure"
-  # net_profile_dns_service_ip     = "10.0.0.10"
-  # net_profile_docker_bridge_cidr = "170.10.0.1/16"
-  net_profile_service_cidr       = "192.168.102.0/24"
-
-  # depends_on = [module.network]
-}
+# module "aks" {
+#   source                           = "Azure/aks/azurerm"
+#   resource_group_name              = data.azurerm_resource_group.tf-hc-prod.name
+#   client_id                        = data.vault_generic_secret.azure.data["client_id"]
+#   client_secret                    = data.vault_generic_secret.azure.data["secret"]
+#   kubernetes_version               = "1.19.3"
+#   orchestrator_version             = "1.19.3"
+#   prefix                           = "tf-aks"
+#   network_plugin                   = "azure"
+#   vnet_subnet_id                   = data.azurerm_virtual_network.tf-hc-prod.id
+#   os_disk_size_gb                  = 50
+#   sku_tier                         = "Paid" # defaults to Free
+#   enable_role_based_access_control = true
+#   rbac_aad_admin_group_object_ids  = [data.azuread_group.cluster-admins.id]
+#   rbac_aad_managed                 = true
+#   private_cluster_enabled          = true # default value
+#   enable_http_application_routing  = true
+#   enable_azure_policy              = true
+#   enable_auto_scaling              = true
+#   agents_min_count                 = 1
+#   agents_max_count                 = 2
+#   agents_count                     = null # Please set `agents_count` `null` while `enable_auto_scaling` is `true` to avoid possible `agents_count` changes.
+#   agents_max_pods                  = 100
+#   agents_pool_name                 = "exnodepool"
+#   # agents_availability_zones        = ["1", "2"]
+#   agents_type                      = "VirtualMachineScaleSets"
+#   agents_size                      = "Standard DS1 v2"
+#
+#   agents_labels = {
+#     "nodepool" : "defaultnodepool"
+#   }
+#
+#   agents_tags = {
+#     "Agent" : "defaultnodepoolagent"
+#   }
+#
+#   network_policy                 = "azure"
+#   # net_profile_dns_service_ip     = "10.0.0.10"
+#   # net_profile_docker_bridge_cidr = "170.10.0.1/16"
+#   net_profile_service_cidr       = "192.168.102.0/24"
+#
+#   # depends_on = [module.network]
+# }
